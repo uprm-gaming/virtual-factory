370f0942012e94a89ab939e6becf9384a9e4d136
Jose F Martinez <jose.martinez70@upr.edu>
2013-10-31 02:32:09 -0400

    Improved the ability of 'picking' objects in the scene.

--- src/edu/uprm/gaming/GameEngine.java
+++ src/edu/uprm/gaming/GameEngine.java
@@ -1300,11 +1300,11 @@ public class GameEngine extends AbstractAppState implements AnimEventListener, P
     };
     
     private void handlePickedObject(String pickingType) {
-        getGeneralScreenController().hideCurrentControlsWindow();
         CollisionResults results = new CollisionResults();
         Ray ray;
         if (pickingType.equals("Picking")) { //Picking with the SHIFT Button
             ray = new Ray(cam.getLocation(), cam.getDirection());
+            getGeneralScreenController().hideCurrentControlsWindow();
         }
         else { //Picking with mouse
             Vector3f origin    = cam.getWorldCoordinates(inputManager.getCursorPosition(), 0.0f);
