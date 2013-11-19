e77353d22aa0b00b858c30ef178be59fb155a62d
Abner Coimbre <abner.coimbre@upr.edu>
2013-11-19 18:24:49 -0400

    making the intent of the code clearer

--- src/edu/uprm/gaming/ScreenSettings.java
+++ src/edu/uprm/gaming/ScreenSettings.java
@@ -11,24 +11,87 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class ScreenSettings {
-    private AppSettings customScreen;
-    
-    public static AppSettings generate() {
-        ScreenSettings s = new ScreenSettings();
-        return s.getAppSettings();
+    private GraphicsDevice computerScreen;
+    private AppSettings customJmonkeyScreen;
+    private DisplayMode selectedScreenMode;
+
+    private ScreenSettings() {
+        loadComputerScreenMode();
+        loadJmonkeyScreen();
+        loadIcons();
     }
     
-    private ScreenSettings() {
-        customScreen = new AppSettings(true);
-        loadIcons();
-        loadDefaultSettings();
-        tryFullScreen();
+    public static AppSettings generate() {
+        ScreenSettings virtualFactoryScreen = new ScreenSettings();
+        return virtualFactoryScreen.getJmonkeyScreen();
+    }
+    
+    private void loadComputerScreenMode() {
+        computerScreen = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        if (!computerScreen.isFullScreenSupported())
+            return;
+        
+        DisplayMode[] availableScreenModes = computerScreen.getDisplayModes();
+        ArrayList<DisplayMode> allowedScreenModes = new ArrayList<>();
+        
+        for (DisplayMode mode: availableScreenModes) {
+            if (mode.getWidth() == 1280)
+                allowedScreenModes.add(mode);
+        }
+        
+        if (!allowedScreenModes.isEmpty()) {
+            selectedScreenMode = allowedScreenModes.get(0);
+            if (allowedScreenModes.size() > 1) {
+                chooseBestScreenMode(allowedScreenModes);
+            }
+        }
+    }
+    
+    private void chooseBestScreenMode(ArrayList<DisplayMode> allowedScreenModes) {
+        for (int i = 0; i < allowedScreenModes.size() - 1; i++) {
+            DisplayMode nextAvailableMode = allowedScreenModes.get(i+1);
+            boolean nextModeHasBetterHeight = Math.abs(selectedScreenMode.getHeight() - 720) 
+                                              > Math.abs(nextAvailableMode.getHeight() - 720);
+            if (nextModeHasBetterHeight)
+                selectedScreenMode = nextAvailableMode;
+        }
+    }
+    
+    private void loadJmonkeyScreen() {
+        customJmonkeyScreen = new AppSettings(true);
+        Params.renderer = customJmonkeyScreen.getRenderer();
+        
+        customJmonkeyScreen.setTitle("Virtual Factory 2.0 (Alpha) - ININ-UPRM - NSF #0835990");
+        customJmonkeyScreen.setRenderer(AppSettings.LWJGL_OPENGL2);
+        customJmonkeyScreen.setSettingsDialogImage("Interface/icon.png");
+        
+        if (isComputerCapableForFullscreen()) {
+            loadFullscreenSettings();
+        }   
+        else
+            loadDefaultScreenSettings();
+    }
+    
+    private boolean isComputerCapableForFullscreen() {
+        return selectedScreenMode != null;
+    }
+    
+    private void loadDefaultScreenSettings() {
+        customJmonkeyScreen.setFullscreen(false);
+        customJmonkeyScreen.setResolution(1280, 720);
+    }
+
+    private void loadFullscreenSettings() {
+        customJmonkeyScreen.setFullscreen(true);
+        customJmonkeyScreen.setWidth(selectedScreenMode.getWidth());
+        customJmonkeyScreen.setHeight(selectedScreenMode.getHeight());
+        customJmonkeyScreen.setFrequency(selectedScreenMode.getRefreshRate());
     }
 
     private void loadIcons() {
         try {
             Class<VirtualFactory> myClass = VirtualFactory.class;
-            customScreen.setIcons(new BufferedImage[]{
+            customJmonkeyScreen.setIcons(new BufferedImage[]{
                 ImageIO.read(myClass.getResourceAsStream("/Textures/icon_16.png")),
                 ImageIO.read(myClass.getResourceAsStream("/Textures/icon_24.png")),
                 ImageIO.read(myClass.getResourceAsStream("/Textures/icon_32.png")),
@@ -37,64 +100,15 @@ public class ScreenSettings {
                 ImageIO.read(myClass.getResourceAsStream("/Textures/icon_72.png"))
             });
         }
+        catch (NullPointerException e) {
+            throw new NullPointerException("customJmonkeyScreen was not initialized - " + e.getMessage());
+        }
         catch (IOException e) {
             System.out.println("Unable to load program icons - " + e.getMessage());
         }
     }
     
-    private void loadDefaultSettings() {
-        Params.renderer = customScreen.getRenderer();
-        customScreen.setResolution(1280, 720);
-        customScreen.setTitle("Virtual Factory 2.0 (Alpha) - ININ-UPRM - NSF #0835990");
-        customScreen.setRenderer(AppSettings.LWJGL_OPENGL2);
-        customScreen.setSettingsDialogImage("Interface/icon.png");
-    }
-    
-    private void tryFullScreen() {
-        DisplayMode mode = getDisplayParams();
-        if (mode != null)
-            setDisplaySettings(customScreen, mode);
-    }
-    
-    //returns null if fullscreen is not available for the display
-    private static DisplayMode getDisplayParams() {
-        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        DisplayMode[] displayModes = device.getDisplayModes();
-        ArrayList<DisplayMode> tempModes = new ArrayList<>();
-        DisplayMode selectedMode;
-        
-        for (DisplayMode mode: displayModes) {
-            if (!device.isFullScreenSupported())
-                return null;
-            if (mode.getWidth() == 1280)
-                tempModes.add(mode);
-        }
-        
-        if (tempModes.isEmpty())
-            return null;
-        else {
-            selectedMode = tempModes.get(0);
-            for (int i = 0; i < tempModes.size(); i++) {
-                if (tempModes.size() == i + 1)
-                    break;
-                
-                DisplayMode temp = tempModes.get(i+1);
-                if (Math.abs(selectedMode.getHeight() - 720) > Math.abs(temp.getHeight() - 720))
-                    selectedMode = temp;
-            }
-        }
-        
-        return selectedMode;
-    }
-    
-    private static void setDisplaySettings(AppSettings settings, DisplayMode mode) {
-        settings.setFullscreen(true);
-        settings.setWidth(mode.getWidth());
-        settings.setHeight(mode.getHeight());
-        settings.setFrequency(mode.getRefreshRate());
-    }
-    
-    public AppSettings getAppSettings() { 
-        return customScreen; 
+    public AppSettings getJmonkeyScreen() { 
+        return customJmonkeyScreen; 
     }
 }
