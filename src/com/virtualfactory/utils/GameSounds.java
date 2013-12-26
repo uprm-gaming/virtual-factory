8378c19005270d7f20c0a056332e6e69a9f794f6
Abner Coimbre <abner.coimbre@upr.edu>
2013-12-26 17:30:13 -0400

    removing unnecessary code

--- src/com/virtualfactory/utils/GameSounds.java
+++ src/com/virtualfactory/utils/GameSounds.java
@@ -1,13 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.virtualfactory.utils;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.audio.AudioNode;
 import com.virtualfactory.engine.GameEngine;
-import java.util.ArrayList;
 
 /**
  *
