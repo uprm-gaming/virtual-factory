6bfe269f7ab32d8dc9154e020ae5e7bb9f6d05ab
Jose F Martinez <jose.martinez70@upr.edu>
2014-01-05 11:47:04 -0400

    New credits!!

--- src/com/virtualfactory/screen/other/Credits.java
+++ src/com/virtualfactory/screen/other/Credits.java
@@ -47,9 +47,9 @@ final public class Credits {
                         });
                         onActiveEffect(new EffectBuilder("gradient") {
                             {
-                                effectValue("offset", "0%", "color", "#00bffecc");
+                                effectValue("offset", "0%", "color", "#00000f");
                                 effectValue("offset", "75%", "color", "#00213cff");
-                                effectValue("offset", "100%", "color", "#880000cc");
+                                effectValue("offset", "100%", "color", "#cccccc");
                             }
                         });
                         onActiveEffect(new EffectBuilder("playSound") {
@@ -73,28 +73,15 @@ final public class Credits {
                                             {
                                                 length(20000);//100000
                                                 effectParameter("start", "0");
-                                                effectParameter("end", "-3200");
+                                                effectParameter("end", "-2350");
                                                 inherit(true);
                                             }
                                         });
                                         panel(common.vspacer("800px"));
                                         text(new TextBuilder() {
                                             {
-                                                text("Nifty 1.3");
+                                                text("Virtual Factory 2.0 (Beta Release)");
                                                 style("creditsCaption");
-                                            }
-                                        });
-                                        text(new TextBuilder() {
-                                            {
-                                                text("Standard Controls Demonstration using JavaBuilder pattern");
-                                                style("creditsCenter");
-                                            }
-                                        });
-                                        panel(common.vspacer("30px"));
-                                        text(new TextBuilder() {
-                                            {
-                                                text("\"Look ma, No XML!\" :)");
-                                                style("creditsCenter");
                                             }
                                         });
                                         panel(common.vspacer("70px"));
@@ -116,7 +103,7 @@ final public class Credits {
                                                                 childLayoutCenter();
                                                                 text(new TextBuilder() {
                                                                     {
-                                                                        text("Nifty 1.3 Core");
+                                                                        text("");
                                                                         style("base-font");
                                                                         alignCenter();
                                                                         valignCenter();
@@ -133,12 +120,9 @@ final public class Credits {
                                                                 childLayoutOverlay();
                                                                 image(new ImageBuilder() {
                                                                     {
-                                                                        filename("Interface/yin.png");
-                                                                    }
-                                                                });
-                                                                image(new ImageBuilder() {
-                                                                    {
-                                                                        filename("Interface/yang.png");
+                                                                        filename("Interface/Logo/VF-icon.png");
+                                                                        width("256px");
+                                                                        height("256px");
                                                                     }
                                                                 });
                                                             }
@@ -150,7 +134,7 @@ final public class Credits {
                                                                 childLayoutCenter();
                                                                 text(new TextBuilder() {
                                                                     {
-                                                                        text("Nifty 1.3 Standard Controls");
+                                                                        text("");
                                                                         style("base-font");
                                                                         alignCenter();
                                                                         valignCenter();
@@ -165,20 +149,20 @@ final public class Credits {
                                         panel(common.vspacer("70px"));
                                         text(new TextBuilder() {
                                             {
-                                                text("written and performed\nby void");
+                                                text("");
                                                 style("creditsCenter");
                                             }
                                         });
                                         panel(common.vspacer("100px"));
                                         text(new TextBuilder() {
                                             {
-                                                text("Sound Credits");
+                                                text("UPRM Gaming team");
                                                 style("creditsCaption");
                                             }
                                         });
                                         text(new TextBuilder() {
                                             {
-                                                text("This demonstration uses creative commons licenced sound samples\nand music from the following sources");
+                                                text("Video game developed by the UPRM gaming team");
                                                 style("creditsCenter");
                                             }
                                         });
@@ -186,92 +170,100 @@ final public class Credits {
                                         image(new ImageBuilder() {
                                             {
                                                 style("creditsImage");
-                                                filename("Interface/freesound.png");
+                                                filename("Interface/Logo/GamingLogo.png");
+                                                width("256px");
+                                                height("256px");
                                             }
                                         });
                                         panel(common.vspacer("25px"));
                                         text(new TextBuilder() {
                                             {
-                                                text("Interface/19546__tobi123__Gong_mf2.wav");
+                                                text("http://ingenieria.uprm.edu/gaming/");
                                                 style("creditsCenter");
                                             }
                                         });
-                                        panel(common.vspacer("50px"));
+                                        panel(common.vspacer("100px"));
                                         image(new ImageBuilder() {
                                             {
                                                 style("creditsImage");
-                                                filename("Interface/cc-mixter-logo.png");
-                                                set("action", "openLink(http://ccmixter.org/)");
+                                                filename("Interface/Logo/NSF_logo.png");
+                                                width("256px");
+                                                height("256px");
+
                                             }
                                         });
                                         panel(common.vspacer("25px"));
                                         text(new TextBuilder() {
                                             {
-                                                text("\"Almost Given Up\" by Loveshadow");
+                                                text("National Sciene Foundation\n"
+                                                        + "#0835990");
                                                 style("creditsCenter");
                                             }
                                         });
                                         panel(common.vspacer("100px"));
-                                        text(new TextBuilder() {
-                                            {
-                                                text("Additional Credits");
-                                                style("creditsCaption");
-                                            }
-                                        });
-                                        text(new TextBuilder() {
-                                            {
-                                                text("ueber awesome Yin/Yang graphic by Dori\n(http://www.nadori.de)\n\nThanks! :)");
-                                                style("creditsCenter");
-                                            }
-                                        });
-                                        panel(common.vspacer("100px"));
-                                        text(new TextBuilder() {
-                                            {
-                                                text("Special thanks go to");
-                                                style("creditsCaption");
-                                            }
-                                        });
-                                        text(new TextBuilder() {
-                                            {
-                                                text(
-                                                        "The following people helped creating Nifty with valuable feedback,\nfixing bugs or sending patches.\n(in no particular order)\n\n"
-                                                        + "chaz0x0\n"
-                                                        + "Tumaini\n"
-                                                        + "arielsan\n"
-                                                        + "gaba1978\n"
-                                                        + "ractoc\n"
-                                                        + "bonechilla\n"
-                                                        + "mdeletrain\n"
-                                                        + "mulov\n"
-                                                        + "gouessej\n");
-                                                style("creditsCenter");
-                                            }
-                                        });
-                                        panel(common.vspacer("75px"));
-                                        text(new TextBuilder() {
-                                            {
-                                                text("Greetings and kudos go out to");
-                                                style("creditsCaption");
-                                            }
-                                        });
-                                        text(new TextBuilder() {
-                                            {
-                                                text(
-                                                        "Ariel Coppes and Ruben Garat of Gemserk\n(http://blog.gemserk.com/)\n\n\n"
-                                                        + "Erlend, Kirill, Normen, Skye and Ruth of jMonkeyEngine\n(http://www.jmonkeyengine.com/home/)\n\n\n"
-                                                        + "Brian Matzon, Elias Naur, Caspian Rychlik-Prince for lwjgl\n(http://www.lwjgl.org/\n\n\n"
-                                                        + "KappaOne, MatthiasM, aho, Dragonene, darkprophet, appel, woogley, Riven, NoobFukaire\nfor valuable input and discussions at #lwjgl IRC on the freenode network\n\n\n"
-                                                        + "... and Kevin Glass\n(http://slick.cokeandcode.com/)\n\n\n\n\n\n\n\n"
-                                                        + "As well as everybody that has not yet given up on Nifty =)\n\n"
-                                                        + "And again sorry to all of you that I've forgotten. You rock too!\n\n\n");
-                                                style("creditsCenter");
-                                            }
-                                        });
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text("Current Team");
+//                                                style("creditsCaption");
+//                                            }
+//                                        });
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text("ueber awesome Yin/Yang graphic by Dori\n(http://www.nadori.de)\n\nThanks! :)");
+//                                                style("creditsCenter");
+//                                            }
+//                                        });
+//                                        panel(common.vspacer("100px"));
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text("Special thanks go to");
+//                                                style("creditsCaption");
+//                                            }
+//                                        });
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text(
+//                                                        "The following people helped creating Nifty with valuable feedback,\nfixing bugs or sending patches.\n(in no particular order)\n\n"
+//                                                        + "chaz0x0\n"
+//                                                        + "Tumaini\n"
+//                                                        + "arielsan\n"
+//                                                        + "gaba1978\n"
+//                                                        + "ractoc\n"
+//                                                        + "bonechilla\n"
+//                                                        + "mdeletrain\n"
+//                                                        + "mulov\n"
+//                                                        + "gouessej\n");
+//                                                style("creditsCenter");
+//                                            }
+//                                        });
+//                                        panel(common.vspacer("75px"));
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text("Greetings and kudos go out to");
+//                                                style("creditsCaption");
+//                                            }
+//                                        });
+//                                        text(new TextBuilder() {
+//                                            {
+//                                                text(
+//                                                        "Ariel Coppes and Ruben Garat of Gemserk\n(http://blog.gemserk.com/)\n\n\n"
+//                                                        + "Erlend, Kirill, Normen, Skye and Ruth of jMonkeyEngine\n(http://www.jmonkeyengine.com/home/)\n\n\n"
+//                                                        + "Brian Matzon, Elias Naur, Caspian Rychlik-Prince for lwjgl\n(http://www.lwjgl.org/\n\n\n"
+//                                                        + "KappaOne, MatthiasM, aho, Dragonene, darkprophet, appel, woogley, Riven, NoobFukaire\nfor valuable input and discussions at #lwjgl IRC on the freenode network\n\n\n"
+//                                                        + "... and Kevin Glass\n(http://slick.cokeandcode.com/)\n\n\n\n\n\n\n\n"
+//                                                        + "As well as everybody that has not yet given up on Nifty =)\n\n"
+//                                                        + "And again sorry to all of you that I've forgotten. You rock too!\n\n\n");
+//                                                style("creditsCenter");
+//                                            }
+                                        //                                        });
                                         panel(common.vspacer("350px"));
                                         image(new ImageBuilder() {
                                             {
                                                 style("creditsImage");
-                                                filename("Interface/nifty-logo.png");
+                                                filename("Interface/VF-Letters-Orange.png");
+                                                width("600px");
+                                                height("146px");
+
                                             }
                                         });
                                     }
